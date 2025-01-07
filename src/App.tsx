3b00362317a8bd83bde38ec7915d76b117077dc4
혀느현스 <hyuns@hyuns.dev>
2025-01-07 14:09:07 +0900

    feat: 2025 다시쌓기, 디자인 일부 수정

--- src/App.tsx
+++ src/App.tsx
@@ -4,45 +4,45 @@ import { Subtitle } from "./components/Subtitle";
 import { Title } from "./components/Title";
 
 const FlexCenter = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Subtitles = styled.div`
-    width: 100%;
-    overflow: hidden;
+  width: 100%;
+  overflow: hidden;
 `;
 
 function App() {
-    return (
-        <>
-            <Title />
-            <Subtitles>
-                <Background>
-                    <FlexCenter>
-                        <Subtitle
-                            text="외적성장"
-                            delay="0.3s"
-                            size="15vw"
-                            period="2024"
-                        />
-                    </FlexCenter>
-                </Background>
-                <Background>
-                    <FlexCenter>
-                        <Subtitle
-                            text="더많은더넓은"
-                            delay="0.2s"
-                            size="15vw"
-                            period="2023"
-                        />
-                    </FlexCenter>
-                </Background>
-            </Subtitles>
-        </>
-    );
+  return (
+    <>
+      <Title />
+      <Subtitles>
+        <Background>
+          <FlexCenter>
+            <Subtitle text="다시쌓기" delay="0.3s" size="15vw" period="2025" />
+          </FlexCenter>
+        </Background>
+        <Background>
+          <FlexCenter>
+            <Subtitle text="외적성장" delay="0.3s" size="15vw" period="2024" />
+          </FlexCenter>
+        </Background>
+        <Background>
+          <FlexCenter>
+            <Subtitle
+              text="더많은더넓은"
+              delay="0.2s"
+              size="15vw"
+              period="2023"
+            />
+          </FlexCenter>
+        </Background>
+      </Subtitles>
+    </>
+  );
 }
 
 export default App;
--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,57 +1,57 @@
 import styled from "styled-components";
 
 const Divver = styled.div`
-    position: fixed;
-    top: 0px;
-    left: 0;
-    width: 100dvw;
-    z-index: 1;
-    justify-content: center;
+  position: fixed;
+  top: 0px;
+  left: 0;
+  width: 100dvw;
+  z-index: 1;
+  justify-content: center;
 
-    height: 96px;
-    backdrop-filter: blur(10px);
+  height: 96px;
+  backdrop-filter: blur(10px);
 `;
 
 const Inner = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 96px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 96px;
 `;
 
 const StyledTitle = styled.h1`
-    font-size: 1rem;
-    text-align: center;
-    color: #ffffff;
-    opacity: 0.5;
-    font-weight: 500;
+  font-size: 1rem;
+  text-align: center;
+  color: #ffffff;
+  opacity: 0.5;
+  font-weight: 500;
 `;
 
 const TitleLabel = styled.span`
-    opacity: 0.7;
+  opacity: 0.8;
 `;
 
 const Author = styled.p`
-    font-size: 1rem;
-    text-align: center;
-    color: #ffffff;
-    opacity: 0.5;
-    font-weight: 500;
+  font-size: 1rem;
+  text-align: center;
+  color: #ffffff;
+  opacity: 0.5;
+  font-weight: 500;
 `;
 
 export function Title() {
-    return (
-        <Divver>
-            <Inner>
-                <StyledTitle>
-                    <TitleLabel>Title - </TitleLabel>새로운 상상을 하고, 상상을
-                    현실로 만듭니다.
-                </StyledTitle>
-                <Author>
-                    <TitleLabel>Author - </TitleLabel>박현우
-                </Author>
-            </Inner>
-        </Divver>
-    );
+  return (
+    <Divver>
+      <Inner>
+        <StyledTitle>
+          <TitleLabel>「 </TitleLabel>새로운 상상을 하고, 상상을 현실로 만듭니다
+          <TitleLabel> 」</TitleLabel>
+        </StyledTitle>
+        <Author>
+          <TitleLabel>Author </TitleLabel>박현우
+        </Author>
+      </Inner>
+    </Divver>
+  );
 }
