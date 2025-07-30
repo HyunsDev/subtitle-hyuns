a24aed95d3539460a62bea14b794cb0f69ec8ca3
혀느현스 <hyuns@hyuns.dev>
2025-07-30 15:09:10 +0900

    feat: description 추가

--- src/App.tsx
+++ src/App.tsx
@@ -22,12 +22,38 @@ function App() {
       <Subtitles>
         <Background>
           <FlexCenter>
-            <Subtitle text="다시쌓기" delay="0.3s" size="15vw" period="2025" />
+            <Subtitle
+              text="다시쌓기"
+              delay="0.3s"
+              size="15vw"
+              period="2025"
+              description={
+                <>
+                  이상(理想)은 머나먼 하늘 그 너머에 있다.
+                  <br />
+                  그곳에 닿기 위해선, 익숙했던 토대를 무너뜨리고 스스로의
+                  확신마저 부정해야 한다.
+                  <br /> 아직 형체조차 가늠할 수 없는 그 지점을 향해, 묵묵히 첫
+                  삽을 들어 올린다."
+                </>
+              }
+            />
           </FlexCenter>
         </Background>
         <Background>
           <FlexCenter>
-            <Subtitle text="외적성장" delay="0.3s" size="15vw" period="2024" />
+            <Subtitle
+              text="외적성장"
+              delay="0.3s"
+              size="15vw"
+              period="2024"
+              description={
+                <>
+                  성장은 그릇의 확장으로부터 비롯된다. <br />더 넓은 세상을 담기
+                  위해선, 먼저 스스로의 틀부터 넓혀야 한다.
+                </>
+              }
+            />
           </FlexCenter>
         </Background>
         <Background>
@@ -37,6 +63,14 @@ function App() {
               delay="0.2s"
               size="15vw"
               period="2023"
+              description={
+                <>
+                  그 여우는 더 이상 우물 속에 갇힌 존재가 아니었다. <br />
+                  처음 마주한 광활한 세계에 매혹된 그 눈동자는,
+                  <br />
+                  이제 그 너머에 자신이 나아갈 길을 묻고 있었다.
+                </>
+              }
             />
           </FlexCenter>
         </Background>
--- src/components/Subtitle/index.tsx
+++ src/components/Subtitle/index.tsx
@@ -1,73 +1,85 @@
 import { styled } from "styled-components";
 
 const Divver = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 0.5rem;
-    width: 100vw;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  width: 100vw;
 `;
 
 const Letters = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Letter = styled.span<{ index: number; size: string; delay: string }>`
-    font-size: 8rem;
-    @keyframes variable-loop {
-        0% {
-            font-weight: 45;
-        }
-        50% {
-            font-weight: 930;
-        }
-        100% {
-            font-weight: 45;
-        }
+  font-size: 8rem;
+  @keyframes variable-loop {
+    0% {
+      font-weight: 45;
     }
-    --delay: calc((${(props) => props.index} + 1) * ${(props) => props.delay});
-    animation: variable-loop 4s infinite both;
-    animation-delay: var(--delay);
-    text-align: center;
+    50% {
+      font-weight: 930;
+    }
+    100% {
+      font-weight: 45;
+    }
+  }
+  --delay: calc((${(props) => props.index} + 1) * ${(props) => props.delay});
+  animation: variable-loop 4s infinite both;
+  animation-delay: var(--delay);
+  text-align: center;
 
-    font-size: ${(props) => props.size};
-    line-height: ${(props) => props.size};
+  font-size: ${(props) => props.size};
+  line-height: ${(props) => props.size};
 
-    font-feature-settings: "calt";
-    will-change: scroll-position;
+  font-feature-settings: "calt";
+  will-change: scroll-position;
 `;
 
 const Label = styled.p`
-    font-size: 1rem;
-    text-align: center;
-    color: #ffffff;
-    font-weight: 500;
-    opacity: 0.8;
+  font-size: 1rem;
+  text-align: center;
+  color: #ffffff;
+  font-weight: 500;
+  opacity: 0.8;
+`;
+
+const Description = styled.p`
+  font-size: 0.7rem;
+  text-align: center;
+  color: #ffffff;
+  font-weight: 500;
+  opacity: 0.6;
+  margin-top: 0.5rem;
 `;
 
 export function Subtitle({
-    text,
-    size,
-    delay,
-    period,
+  text,
+  size,
+  delay,
+  period,
+  description = <></>,
 }: {
-    text: string;
-    size: string;
-    delay: string;
-    period: string;
+  text: string;
+  size: string;
+  delay: string;
+  period: string;
+  description?: string | React.ReactNode;
 }) {
-    return (
-        <Divver>
-            <Label>Subtitle for {period}</Label>
-            <Letters>
-                {text.split("").map((letter, index) => (
-                    <Letter key={index} index={index} size={size} delay={delay}>
-                        {letter}
-                    </Letter>
-                ))}
-            </Letters>
-        </Divver>
-    );
+  return (
+    <Divver>
+      <Label>Subtitle for {period}</Label>
+      <Letters>
+        {text.split("").map((letter, index) => (
+          <Letter key={index} index={index} size={size} delay={delay}>
+            {letter}
+          </Letter>
+        ))}
+      </Letters>
+      {description && <Description>{description}</Description>}
+    </Divver>
+  );
 }
